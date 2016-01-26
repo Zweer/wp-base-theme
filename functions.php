0b1b14b3aacc0251371e2419895400b4ab95dd46
Zwee <flicofloc@gmail.com>
2016-01-26 22:25:55 +0100

    Added the theme slug

--- functions.php
+++ functions.php
@@ -11,6 +11,16 @@ define('WP_THEME_PATH', get_template_directory());
 define('WP_THEME_URI', get_template_directory_uri());
 
 /**
+ * Defining a constant for the theme slug
+ *
+ * In this way we don't have to remember the theme slug every time we need it,
+ * and, much more important, don't have to change it every time we begin a new
+ * project!!
+ */
+
+define('WP_THEME_SLUG', 'base_theme');
+
+/**
  * Including auxiliary files
  *
  * All these files are placed in the 'inc' directory.
